a16b2a99d9487284103008bda96dcc08b706fd86
krishkumarchaurasiya35-design <krishkumarchaurasiya35@gmail.com>
2025-11-27 12:33:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ import { getFirestore, doc, getDoc, setDoc, updateDoc, collection, query, where,
 // Global variables provided by the platform
 const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-app-id';
 const firebaseConfig = {
-    apiKey: "",
+    apiKey: "AIzaSyA1w7fPrXlw_I9tZYvzBTOfLKoJvjNrjPY",
     authDomain: "krish-11166.firebaseapp.com",
     projectId: "krish-11166",
     storageBucket: "krish-11166.firebasestorage.app",
@@ -2472,4 +2472,5 @@ document.addEventListener('DOMContentLoaded', () => {
         loadLandingPageData();
     }
 });
+
 
